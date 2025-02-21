d264a602aa93b3020808eb82d60dd62e4bd74ece
Violeta Kukic <vkukic99@gmail.com>
2025-02-21 12:02:45 +0100

    Fix unused variables and apply TypeScript fixes

--- src/components/CakeModel.tsx
+++ src/components/CakeModel.tsx
@@ -4,7 +4,7 @@ import { Sphere, Cylinder, Torus } from "@react-three/drei";
 interface CakeProps {
   color: string;
   shape: string;
-  decoration: string;
+  decoration?: string;
 }
 
 export default function CakeModel({ color, shape, decoration }: CakeProps) {
@@ -15,6 +15,7 @@ export default function CakeModel({ color, shape, decoration }: CakeProps) {
       {shape === "Round" && <Sphere args={[1, 32, 32]} position={[0, 0, 0]} />}
       {shape === "Square" && <Cylinder args={[1, 1, 1, 4]} position={[0, 0, 0]} />}
       {shape === "Heart" && <Torus args={[0.5, 0.2, 16, 100]} position={[0, 0, 0]} />}
+      {/* Future logic for applying decoration can be added here */}
     </Canvas>
   );
 }
--- src/components/models/HeartCake.tsx
+++ src/components/models/HeartCake.tsx
@@ -9,10 +9,12 @@ interface HeartCakeProps {
 
 export default function HeartCake({ color, cakeRef }: HeartCakeProps) {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Torus args={[0.5, 0.2, 16, 100]} position={[0, 0, 0]} />
-    </Canvas>
+    <div ref={cakeRef}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Torus args={[0.5, 0.2, 16, 100]} position={[0, 0, 0]} />
+      </Canvas>
+    </div>
   );
 }
